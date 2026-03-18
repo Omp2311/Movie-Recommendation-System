286f15bac64fb99dc8b7b3df8d3d6b6552cb811e
Omp2311 <omprakashraj100078@gmail.com>
2026-03-18 22:25:54 +0530

    changes

--- app.py
+++ app.py
@@ -1,33 +1,39 @@
 import streamlit as st
 import pickle
 import joblib
-import nltk
 import pandas as pd
-import sklearn
+
 st.title("Movie Recommendation System")
 
-with open("movies.pickle", 'rb') as m:
-    movies = pickle.load(m)
+@st.cache_data
+def load_movies():
+    with open("movies.pickle", "rb") as m:
+        return pickle.load(m)
 
-similarites = joblib.load("similarity.joblib")
-movie_name = movies['title'].values
+@st.cache_resource
+def load_similarity():
+    return joblib.load("similarity.joblib")
+
+movies = load_movies()
+similarities = load_similarity()
+
+movie_name = movies["title"].values
 
 def recommend(name_movies):
-    movie_index = movies[movies['title'] == name_movies].index[0]
-    recommendation = similarites[movie_index]
-    movie_list = sorted(enumerate(recommendation),reverse=True,key = lambda x:x[1])[1:6]
+    movie_index = movies[movies["title"] == name_movies].index[0]
+    recommendation = similarities[movie_index]
+    movie_list = sorted(enumerate(recommendation), key=lambda x: x[1], reverse=True)[1:6]
+    
     recommend_movies = []
     for i in movie_list:
-        recommend_movies.append(movies.iloc[i[0]].title)
+        recommend_movies.append(movies.iloc[i[0]]["title"])
     return recommend_movies
 
-
-name_movies = st.selectbox("Enter the Movie Name",movie_name)
-
+name_movies = st.selectbox("Enter the Movie Name", movie_name)
 
 if st.button("Recommend"):
     r = recommend(name_movies)
-    st.write("The Recommend Movies are:")
+    st.write("The Recommended Movies are:")
     
     for i in r:
         st.write(i)
